21dc1517507041091c542eb806e4a5f94af41f32
Sameer <92421789+ContactingSameer@users.noreply.github.com>
2024-05-02 11:39:49 +0530

    Add files via upload

---
+++ P7   km to miles.py
@@ -0,0 +1,14 @@
+# Write a Python program to convert Kilometer into miles.
+
+a=int(input('Enter the distance in Kilomete - '))
+a=a*0.621371
+print('\n',a,'miles')
+
+
+
+def kilometre_1(kmeter):   
+conversion_ratio_1= 0.621371   
+miles_1 = kmeter * conversion_ratio_1   
+print ("The speed value of car in Miles: ", miles_1)   
+kmeter = float (input ("Enter the speed of car in Kilometre as a unit: "))   
+kilometre_1(kmeter) 
---
+++ P8   c to f.py
@@ -0,0 +1,18 @@
+# Write a Python program to convert C to F
+
+a=int(input('Enter the Temp in Cels - '))
+f=(a*(9/5))+32
+print('\n',f,'F')
+
+
+
+T(°Fahrenheit)=(T(°Celsius)*1.8)+32 
+T(°Fahrenheit)=(T(°Celsius)*9/5)+32 
+celsius_1 = float(input("Temperature value in degree Celsius: " ))   
+Fahrenheit_1 = (celsius_1 * 1.8) + 32      
+print('The %.2f degree Celsius is equal to: %.2f Fahrenheit'   
+%(celsius_1, Fahrenheit_1))   
+celsius_2 = float (input("Temperature value in degree Celsius: " ))   
+Fahrenheit_2 = (celsius_2 * 9/5) + 32   
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'   
+%(celsius_2, Fahrenheit_2)) 
